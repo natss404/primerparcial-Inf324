74634efeb9b71a9dffdb5b14dab486994df69a2d
natss404 <62126062+natss404@users.noreply.github.com>
2021-10-05 23:30:16 -0400

    Update Lectura2.php
    Primer Parcial

--- pregunta 4/Lectura2.php
+++ pregunta 4/Lectura2.php
@@ -13,5 +13,43 @@ class Lectura2 extends CI_Controller {
 		$this->load->view('myviewLectura', $data);
 	}
 
+	public function eliminar(){
+			$this->load->model("Voyheska_Model");
+		$ci= $this->uri->segment(3);
+		$this->Voyheska_Model->eliminarDocente($ci);
+	}
 	
+	
+		
+	public function editar($ci=null)
+	{
+			
+			if(!$ci == null){
+				$this->load->database();
+				$ci=$this->db->escape((int)$ci);
+				$this->load->model("Voyheska_Model");
+				$doc= $this->Voyheska_Model->getDocente($ci);
+				$this->load->view("editar", compact("doc"));
+			}
+			else {
+				header("Location: Lectura2.php");
+			}
+		
+	
+	}
+
+	
+	public function actualizarDocente(){
+		if($this->input->post()){
+			
+			$this->load->model("Voyheska_Model");
+				$this->load->database();
+			$ci=$this->db->escape((int)$_POST["ci"]);
+			$sigla=$this->db->escape($_POST["sigla"]);
+			if($this->Voyheska_Model->actualizarDocente($ci, $sigla)){
+				 $this->load->helper('url'); 
+				header("Location: https://localhost/code/index.php/Lectura2");
+			}
+		}
+	}
 }
